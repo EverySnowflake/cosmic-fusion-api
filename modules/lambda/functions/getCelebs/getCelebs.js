30c1f6b30318dbbf6ce33d5bc2a82ff963a955af
lucy.borthwick <lucyborthwick95@gmail.com>
2021-02-16 14:26:47 +0000

    Get celebs endpoint

---
+++ modules/lambda/functions/getCelebs/getCelebs.js
@@ -0,0 +1,39 @@
+"use strict";
+
+const mysql = require('mysql');
+const celebsSql = `SELECT name, description, dateOfBirth, sex, animal_id, element_id, western_id, image FROM celebs`;
+const DATABASE_HOST = process.env.DATABASE_HOST;
+const DATABASE_PORT = process.env.DATABASE_PORT;
+const USERNAME = process.env.USERNAME;
+const PASSWORD = process.env.PASSWORD;
+const DATABASE_NAME = process.env.DATABASE_NAME;
+
+exports.handler = (event, context, callback) => {
+
+    const db = mysql.createConnection({
+        host: DATABASE_HOST,
+        port: DATABASE_PORT,
+        user: USERNAME,
+        password: PASSWORD,
+        database: DATABASE_NAME
+    });
+
+    db.connect((error) => {
+        if(error) throw error;
+        console.log("Connected");
+    });
+
+    db.query(celebsSql, function (err, result) {
+        if (err) throw err;
+        const response = {
+            statusCode: 200,
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: result
+        };
+        console.log(result);
+        callback(err, response);
+        db.connection.end(function (err) { callback(err, result);});
+    });
+};

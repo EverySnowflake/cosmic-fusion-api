c9065f5480f742ac5d3f786803e8926b28e7ac83
lucy.borthwick <lucyborthwick95@gmail.com>
2021-02-28 16:07:06 +0000

    Test freidn

--- main.tf
+++ main.tf
@@ -87,6 +87,34 @@ module "get_celebs_endpoint" {
   http_method          = "GET"
   authorization        = "NONE"
   path_part            = "getcelebs"
+  query_string_a       = "dob"
+  query_string_b       = "sex_at_birth"
+  query_string_c       = "friend_dob"
+  query_string_d       = "friend_sex_at_birth"
+  rest_api_id          = module.cosmic_fusion_api.id
+  root_resource_id     = module.cosmic_fusion_api.root_resource_id
+  lambda_function_name = module.get_celebs_lambda.function_name
+  lambda_invoke_arn    = module.get_celebs_lambda.invoke_arn
+}
+
+#GET friend
+
+module "get_celebs_lambda" {
+  source                 = "./modules/lambda"
+  lambda_zip_file        = "${var.env}GetFriend.zip"
+  function_name          = "${var.env}GetFriend"
+  handler                = "getFriend.handler"
+  runtime                = "nodejs12.x"
+  rest_api_execution_arn = module.cosmic_fusion_api.execution_arn
+  lambda_bucket          = var.lambda_bucket
+  env                    = var.env
+}
+
+module "get_celebs_endpoint" {
+  source               = "./modules/endpoints"
+  http_method          = "GET"
+  authorization        = "NONE"
+  path_part            = "getfriend"
   query_string_a       = false
   query_string_b       = false
   query_string_c       = false
@@ -96,4 +124,3 @@ module "get_celebs_endpoint" {
   lambda_function_name = module.get_celebs_lambda.function_name
   lambda_invoke_arn    = module.get_celebs_lambda.invoke_arn
 }
-
